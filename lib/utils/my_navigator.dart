54cb6694ea22fadeec8ba7b386e79246deac191d
Estiverson <40440392+Estiverson@users.noreply.github.com>
2018-06-26 13:07:20 +0200

    Update my_navigator.dart

--- lib/utils/my_navigator.dart
+++ lib/utils/my_navigator.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class MyNavigator {
   static void goToHomeScreen(BuildContext context) {
-    Navigator.pushNamed(context,"/home");
+    Navigator.pushReplacementNamed(context,"/home");
   }
 }
